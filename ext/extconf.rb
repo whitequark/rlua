7b34f64939d7a1785fe92b5998c57c08424e9fcb
mishaelajay <mishaelajay@gmail.com>
2025-01-11 22:32:51 +0530

    Upgrade to lua 5.4

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -2,7 +2,7 @@ require "mkmf"
 
 $CFLAGS += " -std=c99 -Wno-declaration-after-statement"
 
-dir_config('lua5.3')
+dir_config('lua5.4')
 
 unless have_library('lua5.3', 'luaL_newstate') or have_library('lua.5.3', 'luaL_newstate')
   puts ' extconf failure: need liblua5.3'
--- ext/rlua.c
+++ ext/rlua.c
@@ -1,7 +1,7 @@
 #include <ruby.h>
-#include <lua5.3/lua.h>
-#include <lua5.3/lauxlib.h>
-#include <lua5.3/lualib.h>
+#include <lua5.4/lua.h>
+#include <lua5.4/lauxlib.h>
+#include <lua5.4/lualib.h>
 #include <ctype.h>
 #include <ruby/encoding.h>
 
